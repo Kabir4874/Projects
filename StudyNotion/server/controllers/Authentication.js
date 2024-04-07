122f953b67ff4c3dd2e8c1a4d956969bddd26a92
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-07 21:21:21 +0600

    add

--- StudyNotion/server/controllers/Authentication.js
+++ StudyNotion/server/controllers/Authentication.js
@@ -1,11 +1,175 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
+const Profile = require("../models/Profile");
 
-// sendOTP
+// !sendOTP
+exports.sendOTP = async (req, res) => {
+  try {
+    // fetch email from request body
+    const { email } = req.body;
 
+    // check if user already exits
+    const checkUserPresent = await User.findOne({ email });
 
-// signup
+    // if user already exists, then return a response
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User already registered",
+      });
+    }
 
+    // generate otp
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+    console.log("OTP Generated: ", otp);
 
-// login
+    // check unique otp or not
+    let result = await OTP.findOne({ otp: otp });
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+      result = await OTP.findOne({ otp: otp });
+    }
 
+    const otpPayload = { email, otp };
+    // create an entry in db for otp
 
-// change Password 
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+    // return response successful
+    res.status(200).json({
+      success: true,
+      message: "OTP sent Successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+// !signup
+exports.signUp = async (req, res) => {
+  try {
+    // data fetch from request body
+    const {
+      firstName,
+      lastName,
+      email,
+      contactNumber,
+      password,
+      confirmPassword,
+      accountType,
+      otp,
+    } = req.body;
+
+    // validate data
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !contactNumber ||
+      !password ||
+      !confirmPassword ||
+      !otp
+    ) {
+      return res.status(403).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // match 2 password
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Password and ConfirmPassword Value does not match, please try again",
+      });
+    }
+
+    // check user already exits or not
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already registered",
+      });
+    }
+
+    // find most recent OTP Stored for the user
+    const recentOtp = await OTP.find({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    console.log(recentOtp);
+
+    // validate OTP
+    if (recentOtp.length === 0) {
+      // OTP not found
+      return res.status(400).json({
+        success: false,
+        message: "OTP not found",
+      });
+    } else if (otp !== recentOtp.otp) {
+      // Invalid OTP
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create profile
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumber: null,
+    });
+
+    // entry create on DB
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+    });
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "User is registered Successfully",
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "User cannot be registered, Please try again later",
+    });
+  }
+};
+
+// !login
+exports.login = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+// !change Password
--- StudyNotion/server/models/User.js
+++ StudyNotion/server/models/User.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
+  contactNumber: {
+    type: Number,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
