4bc7fcb52bee4128803d6fdb2cecb26453448bad
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-07 19:20:50 +0600

    add

--- StudyNotion/server/models/OTP.js
+++ StudyNotion/server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,25 @@ const OTPSchema = new mongoose.Schema({
     default: Date.now(),
     expires: 5 * 60,
   },
+});
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from StudyNotion",
+      otp
+    );
+    console.log("Email Send Successfully: ", mailResponse);
+  } catch (err) {
+    console.log("Error occurred while sending mails: ", err);
+    throw err;
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
 });
 
 module.exports = mongoose.model("OTP", OTPSchema);
