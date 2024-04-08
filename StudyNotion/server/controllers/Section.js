4b0e939406bcbe835511922b6af02377b0fccb0a
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-08 14:41:22 +0600

    add

---
+++ StudyNotion/server/controllers/Section.js
@@ -0,0 +1,100 @@
+const Section = require("../models/Section");
+const Course = require("../models/Course");
+
+// !Create section
+exports.createSection = async (req, res) => {
+  try {
+    // data fetch
+    const { sectionName, courseId } = req.body;
+
+    // data validation
+    if (!sectionName || !courseId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing properties",
+      });
+    }
+
+    // create section
+    const newSection = await Section.create({ sectionName });
+
+    // update course with section objectId
+    const updatedCourseDetails = await Course.findByIdAndUpdate(
+      courseId,
+      {
+        $push: {
+          courseContent: newSection._id,
+        },
+      },
+      { new: true }
+    );
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Section created successfully",
+      updatedCourseDetails,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+// !update section
+exports.updateSection = async (req, res) => {
+  try {
+    // data input
+    const { sectionName, sectionId } = req.body;
+
+    // data validation
+    if (!sectionName || !sectionId) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing properties",
+      });
+    }
+
+    // update data
+    const section = await Section.findByIdAndUpdate(
+      sectionId,
+      { sectionName },
+      { new: true }
+    );
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Section Updated successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+// delete section
+exports.deleteSection = async (req, res) => {
+  try {
+    // get ID
+    const { sectionId } = req.params;
+
+    // delete section
+    await Section.findByIdAndDelete(sectionId);
+
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Section Deleted successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
