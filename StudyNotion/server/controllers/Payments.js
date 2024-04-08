bfbf87ea18b23a03302892b429f496989a46435f
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-08 21:45:28 +0600

    add

--- StudyNotion/server/controllers/Payments.js
+++ StudyNotion/server/controllers/Payments.js
@@ -5,6 +5,7 @@ const mailSender = require("../utils/mailSender");
 const {
   courseEnrollmentEmail,
 } = require("../mail/templates/courseEnrollmentEmail");
+const mongoose = require("mongoose");
 
 // !capture the payment and initiate razorpay order
 exports.capturePayment = async (req, res) => {
@@ -29,9 +30,60 @@ exports.capturePayment = async (req, res) => {
           message: "Could not find the course",
         });
       }
-    } catch (err) {}
-    // user already pay for the same course
+
+      // user already pay for the same course
+      const uid = new mongoose.Types.ObjectId(userId);
+      if (course.studentsEnrolled.includes(uid)) {
+        return res.status(200).json({
+          success: false,
+          message: "student is already enrolled",
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+
     // order create
-    // return response
-  } catch (err) {}
+    const amount = course.price;
+    const currency = "INR";
+    const options = {
+      amount: amount * 100,
+      currency,
+      receipt: Math.random(Date.now()).toString(),
+      notes: {
+        courseId: course_id,
+        userId,
+      },
+    };
+    try {
+      // initiate the payment using razorpay
+      const paymentResponse = await instance.orders.create(options);
+      console.log(paymentResponse);
+      return res.status(200).json({
+        success: true,
+        courseName: course.courseName,
+        courseDescription: course.courseDescription,
+        thumbnail: course.thumbnail,
+        orderId: paymentResponse.id,
+        currency: paymentResponse.currency,
+        amount: paymentResponse.amount,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
