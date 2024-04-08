af13802e3a02837ada62d8b764828d12318667f3
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-08 20:42:14 +0600

    add

--- StudyNotion/server/models/CourseProgress.js
+++ StudyNotion/server/models/CourseProgress.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const courseProgressSchema = new mongoose.Schema({
-  courseID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-  },
-  completedVideo: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      refer: "SubSection",
-    },
-  ],
-});
+const courseProgress = new mongoose.Schema({
+    courseID : {
+        type : mongoose.SchemaType.Types.ObjectId,
+        ref: "Course",
+    }, 
+    completedVideos : [
+        {
+            type : mongoose.SchemaType.Types.ObjectId,
+            ref : "SubSection"
+        }
+    ]
 
-module.exports = mongoose.model("CourseProgress", courseProgressSchema);
+})
+
+module.exports = mongoose.model("CourseProgress", courseProgress);
