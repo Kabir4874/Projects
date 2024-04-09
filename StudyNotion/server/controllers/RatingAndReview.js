86f4d172dcf00f5f44fa7ace591e087f10381636
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-09 10:18:30 +0600

    add

--- StudyNotion/server/controllers/RatingAndReview.js
+++ StudyNotion/server/controllers/RatingAndReview.js
@@ -1,5 +1,6 @@
 const RatingAndReview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
+const mongoose = require("mongoose");
 
 // !createRating
 exports.createRating = async (req, res) => {
@@ -69,9 +70,36 @@ exports.createRating = async (req, res) => {
 // !getAverageRating
 exports.getAverageRating = async (req, res) => {
   try {
-    // get courseId 
-    // calculate average rating 
-    // return rating 
+    // get courseId
+    const courseId = req.body.courseid;
+
+    // calculate average rating
+    const result = await RatingAndReview.aggregate([
+      {
+        $match: {
+          course: new mongoose.Types.ObjectId(courseId),
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          averageRating: { $avg: "$rating" },
+        },
+      },
+    ]);
+
+    // return rating
+    if (result.length > 0) {
+      return res.status(200).json({
+        success: true,
+        averageRating: result[0].averageRating,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Average Rating is 0, no rating given till now",
+      averageRating: 0,
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -81,3 +109,26 @@ exports.getAverageRating = async (req, res) => {
 };
 
 // !getAllRating
+exports.getAllRating = async (req, res) => {
+  try {
+    const allReviews = await RatingAndReview.find({})
+      .sort({ rating: "desc" })
+      .populate({ path: "user", select: "firstName lastName email image" })
+      .populate({
+        path: "course",
+        select: "courseName",
+      })
+      .exec();
+
+    return res.status(200).json({
+      success: true,
+      message: "All review fetched successfully",
+      data: allReviews,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
