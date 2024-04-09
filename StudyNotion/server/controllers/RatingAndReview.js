64a2cbf4b9d267039fdbd5df28ecf3d1cafb684d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-09 09:48:19 +0600

    add

--- StudyNotion/server/controllers/RatingAndReview.js
+++ StudyNotion/server/controllers/RatingAndReview.js
@@ -5,11 +5,59 @@ const Course = require("../models/Course");
 exports.createRating = async (req, res) => {
   try {
     // get user id
+    const userId = req.user.id;
+    const { rating, review, courseId } = req.body;
+
     // check if user is enrolled or not
+    const courseDetails = await Course.findOne({
+      _id: courseId,
+      studentsEnrolled: { $elemMatch: { $eq: userId } },
+    });
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Student is not enrolled in the Course",
+      });
+    }
+
     // check if user already reviewed the course
+    const alreadyReviewed = await RatingAndReview.findOne({
+      user: userId,
+      course: courseId,
+    });
+    if (alreadyReviewed) {
+      return res.status(403).json({
+        success: false,
+        message: "Course is already reviewed by the user",
+      });
+    }
+
     // create rating and review
+    const ratingReview = await RatingAndReview.create({
+      rating,
+      review,
+      course: courseId,
+      user: userId,
+    });
+
     // update course
+    const updatedCourseDetails = await Course.findByIdAndUpdate(
+      { _id: courseId },
+      {
+        $push: {
+          ratingAndReviews: ratingReview._id,
+        },
+      },
+      { new: true }
+    );
+    console.log(updatedCourseDetails);
+
     // return response
+    return res.status(200).json({
+      success: true,
+      message: "Rating and Review created Successfully",
+      ratingReview,
+    });
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -19,5 +67,17 @@ exports.createRating = async (req, res) => {
 };
 
 // !getAverageRating
+exports.getAverageRating = async (req, res) => {
+  try {
+    // get courseId 
+    // calculate average rating 
+    // return rating 
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
 // !getAllRating
