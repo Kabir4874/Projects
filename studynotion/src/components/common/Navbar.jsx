7ab9b2064942e3410094f49001ed149bb0ad35c3
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-12 18:44:23 +0600

    add

--- studynotion/src/components/common/Navbar.jsx
+++ studynotion/src/components/common/Navbar.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import { Link, matchPath, useLocation } from "react-router-dom";
 import { NavbarLinks } from "../../data/navbar-links";
+import { useSelector } from "react-redux";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import ProfileDropDown from "../core/HomePage/Auth/ProfileDropDown";
+import { apiConnector } from "../../services/apiConnector";
+import { categories } from "../../services/api";
 
 const Navbar = () => {
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
   const location = useLocation();
+
+  const [subLinks, setSubLinks] = useState([]);
+
+  const fetchSubLinks = async () => {
+    try {
+      const result = await apiConnector("GET", categories.CATEGORIES_API);
+      setSubLinks(result.data);
+    } catch (err) {
+      console.log("Could nt fetch the category list");
+    }
+  };
+
+  useEffect(() => {
+    fetchSubLinks();
+  }, []);
+
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
   };
@@ -38,6 +62,33 @@ const Navbar = () => {
             ))}
           </ul>
         </nav>
+
+        {/* Login/ signup/dashboard  */}
+        <div className="flex gap-x-4 items-center">
+          {user && user?.accountType !== "Instructor" && (
+            <Link to={"/dashboard/cart"} className="relative">
+              <AiOutlineShoppingCart />
+              {totalItems > 0 && <span>{totalItems}</span>}
+            </Link>
+          )}
+
+          {token === null && (
+            <Link to={"/login"}>
+              <button className="border border-richBlack-700 bg-richBlack-800 px-3 py-2 text-richBlack-100 rounded-md">
+                Login
+              </button>
+            </Link>
+          )}
+
+          {token === null && (
+            <Link to={"/signup"}>
+              <button className="border border-richBlack-700 bg-richBlack-800 px-3 py-2 text-richBlack-100 rounded-md">
+                Sign Up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropDown />}
+        </div>
       </div>
     </div>
   );
