db616e047556071a4af8a69cf46b3cfa54a30ad3
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-14 11:54:30 +0600

    add

--- ecommerce/dashboard/src/views/auth/Register.jsx
+++ ecommerce/dashboard/src/views/auth/Register.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className=" w-screen h-screen bg-dark flex justify-center items-center">
+      <div className="w-[350px] text-light p-2">
+        <div className="bg-Blue p-4 rounded-md">
+          <h2 className="text-xl mb-3">Welcome to E-commerce</h2>
+          <p className="text-sm mb-3">
+            Please Register and start your business
+          </p>
+          <form action="">
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                id="name"
+                required
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-light focus:border-indigo-500 overflow-hidden"
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                id="email"
+                required
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-light focus:border-indigo-500 overflow-hidden"
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                id="password"
+                required
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-light focus:border-indigo-500 overflow-hidden"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
