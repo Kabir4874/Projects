3a30dcee228e0ced144e25315fb3687eae2713ce
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-16 21:05:35 +0600

    add

--- ecommerce/dashboard/src/views/admin/Category.jsx
+++ ecommerce/dashboard/src/views/admin/Category.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Pagination from "../Pagination";
 
 const Category = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
   const [perPage, setPerPage] = useState(5);
+  const [show, setShow] = useState(false);
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="flex flex-wrap w-full">
@@ -48,26 +53,46 @@ const Category = () => {
                         {item}
                       </td>
                       <td className="py-3 px-4 font-medium whitespace-nowrap">
-                        <img src={`http://localhost:3000/images/category/${item}.jpg`} alt="" />
+                        <img
+                          src={`http://localhost:3000/images/category/${item}.jpg`}
+                          alt="category"
+                          className="w-[45px] h-[45px]"
+                        />
                       </td>
                       <td className="py-3 px-4 font-medium whitespace-nowrap">
-                        <span>pending</span>
+                        <span>Sports</span>
                       </td>
                       <td className="py-3 px-4 font-medium whitespace-nowrap">
-                        <span>pending</span>
-                      </td>
-                      <td className="py-3 px-4 font-medium whitespace-nowrap">
-                        <Link>View</Link>
+                        <div className="flex justify-start items-center gap-4">
+                          <Link className="p-[6px] bg-yellow-600 rounded-sm hover:shadow-lg hover:shadow-yellow-600/50 flex items-center justify-center">
+                            <FaEdit />
+                          </Link>
+                          <Link className="p-[6px] bg-red-500 rounded-sm hover:shadow-lg hover:shadow-red-500/50 flex items-center justify-center">
+                            <FaTrash />
+                          </Link>
+                        </div>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
+            {/* pagination  */}
+            <div className="w-full flex justify-end mt-4 mb-2">
+              <Pagination
+                pageNumber={currentPage}
+                setPageNumber={setCurrentPage}
+                totalItem={50}
+                perPage={perPage}
+                showItem={3}
+              />
+            </div>
           </div>
         </div>
         <div className="w-full lg:w-5/12">
-          <div className="w-full p-4 bg-Blue rounded-md"></div>
+          <div className="w-full p-4 bg-Blue rounded-md">
+            
+          </div>
         </div>
       </div>
     </div>
