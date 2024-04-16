025e8792c8ee451c7df4ef83f3e58ee88a04fe49
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-16 20:25:14 +0600

    add

--- ecommerce/dashboard/src/views/admin/Category.jsx
+++ ecommerce/dashboard/src/views/admin/Category.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Category = () => {
+  const [perPage, setPerPage] = useState(5);
   return (
-    <div>Category</div>
-  )
-}
+    <div className="px-2 lg:px-7 pt-5">
+      <div className="flex flex-wrap w-full">
+        <div className="w-full lg:w-7/12">
+          <div className="w-full p-4 bg-Blue rounded-md">
+            <div className="flex justify-between items-center">
+              <select
+                className="px-4 py-2 focus:border-indigo-500 outline-none bg-Blue border border-slate-700 rounded-md text-light"
+                onChange={(e) => setPerPage(parseInt(e.target.value))}
+              >
+                <option value="5">5</option>
+                <option value="5">15</option>
+                <option value="5">25</option>
+              </select>
+              <input
+                type="text"
+                placeholder="search"
+                className="px-4 py-2 focus:border-indigo-500 outline-none bg-Blue border border-slate-700 rounded-md text-light"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-5/12">
+          <div className="w-full p-4 bg-Blue rounded-md"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
