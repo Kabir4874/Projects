bef107670f5b384bf433d2a8ac2f7b3b8e460e9b
Kabir4874 <kabir.cse.bd@gmail.com>
2024-04-17 10:24:29 +0600

    add

---
+++ ecommerce/dashboard/src/views/admin/PaymentRequest.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FixedSizeList as List} from 'react-window'
+
+const PaymentRequest = () => {
+  return (
+    <div className='px-2 lg:px-7 pt-5'>
+        <div className='w-full bg-Blue rounded-md'>
+        <h2 className='text-xl font-medium pb-5 text-light'>Withdrawal request</h2>
+            <div className='w-full'>
+                <div className='w-full overflow-x-auto'>
+                    <div className='flex bg-dark'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PaymentRequest
