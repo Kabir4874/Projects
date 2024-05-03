aad808ff8fcd1d4577fdaa2588012fa86187f516
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-03 23:06:47 +0600

    add

--- ecommerce/backend/routes/dashboard/productRoutes.js
+++ ecommerce/backend/routes/dashboard/productRoutes.js
@@ -5,7 +5,15 @@ const productController = require("../../controllers/dashboard/productController
 
 router.post("/product-add", authMiddleware, productController.add_product);
 router.get("/products-get", authMiddleware, productController.products_get);
-router.get("/product-get/:productId", authMiddleware, productController.product_get);
-router.post("/product-update/:productId", authMiddleware, productController.product_update);
+router.get(
+  "/product-get/:productId",
+  authMiddleware,
+  productController.product_get
+);
+router.post(
+  "/product-update/",
+  authMiddleware,
+  productController.product_update
+);
 
 module.exports = router;
