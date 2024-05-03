5cc37d5b7f2fb8b040b2544cd6e99344000c4c6e
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-04 00:50:00 +0600

    add

---
+++ PixelPlo/softlight/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#030211",
+        grey: "#7C7C7C",
+        grey_light: "#C1C1C1",
+      },
+    },
+  },
+  plugins: [],
+};
