b0678d0b4e34b0d0ebc62b46a9434b8ed8735465
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-05 21:19:36 +0600

    add

--- PixelPlo/softlight/src/components/Home/Testimonial.jsx
+++ PixelPlo/softlight/src/components/Home/Testimonial.jsx
@@ -1,119 +1,66 @@
-import { testimonialData } from "../../data/homeData";
 import Stroke from "../../assets/Stroke.svg";
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-
-// import required modules
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
-
 import test1 from "../../assets/test1.png";
 import test2 from "../../assets/test2.png";
 import test3 from "../../assets/test3.png";
 import quote from "../../assets/quote.svg";
+import { useState } from "react";
+import { testimonialData } from "../../data/homeData";
 
 const Testimonial = () => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [toggleState, setToggleState] = useState(2);
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <div className="w-[75rem] mx-auto">
-      <Swiper
-        loop={true}
-        spaceBetween={10}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide className="relative">
-          <img src={Stroke} />
-          <div className=" absolute top-[50%] w-[50.875rem] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-[1.62rem]">
-            <div className="w-[2.370rem]">
-              <img src={quote} alt="" className="w-full" />
-            </div>
-            <p className=" text-jaguar text-center font-nunito text-[1.5625rem]">
-              High-end digital experiences. Created at the heart of Manhattan,
-              we are a should human team. The driving force of all speeches, we
-              believe that creation should be the point around which any
-              strategy revolves.
+      <div className=" w-full relative">
+        <img src={Stroke} className="w-full" />
+        <div className=" absolute top-[50%] w-[50.875rem] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-[1.62rem]">
+          <div className="w-[2.370rem]">
+            <img src={quote} alt="" className="w-full" />
+          </div>
+          <p className=" text-jaguar text-center font-nunito text-[1.5625rem]">
+            {testimonialData[toggleState - 1].text}
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-start gap-[45px] mt-5">
+        <div
+          className="flex flex-col items-center justify-center"
+          onClick={() => (toggleState === 1 ? toggleTab(2) : toggleTab(1))}
+        >
+          <img
+            src={toggleState === 1 ? test2 : test1}
+            alt=""
+            className="w-[112px] mt-9"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <img
+            src={testimonialData[toggleState - 1].image}
+            alt=""
+            className="w-[174.67px]"
+          />
+          <div className="mt-4 flex flex-col items-center justify-center">
+            <h4 className=" text-jaguar font-syne text-[1.875rem] font-medium mb-[0.12rem]">
+              {testimonialData[toggleState - 1].name}
+            </h4>
+            <p className=" text-jaguar font-nunito text-lg">
+              {testimonialData[toggleState - 1].designation}
             </p>
           </div>
-        </SwiperSlide>
-        <SwiperSlide className=" relative">
-          <img src={Stroke} />
-          <div className=" absolute top-[50%] w-[50.875rem] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-[1.62rem]">
-            <div className="w-[2.370rem]">
-              <img src={quote} alt="" className="w-full" />
-            </div>
-            <p className=" text-jaguar text-center font-nunito text-[1.5625rem]">
-              High-end digital experiences. Created at the heart of Manhattan,
-              we are a should human team. The driving force of all speeches, we
-              believe that creation should be the point around which any
-              strategy revolves.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className=" relative">
-          <img src={Stroke} />
-          <div className=" absolute top-[50%] w-[50.875rem] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center gap-[1.62rem]">
-            <div className="w-[2.370rem]">
-              <img src={quote} alt="" className="w-full" />
-            </div>
-            <p className=" text-jaguar text-center font-nunito text-[1.5625rem]">
-              High-end digital experiences. Created at the heart of Manhattan,
-              we are a should human team. The driving force of all speeches, we
-              believe that creation should be the point around which any
-              strategy revolves.
-            </p>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        loop={true}
-        spaceBetween={10}
-        slidesPerView={3}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper mt-5"
-      >
-        <SwiperSlide>
-          <div>
-            <img src={test1} alt="" />
-            <div className="mt-4">
-              <h4 className=" text-jaguar font-syne text-[1.875rem] font-medium mb-[0.12rem]">
-                Charl Gems
-              </h4>
-              <p className=" text-jaguar font-nunito text-lg">
-                Founder of HiTechs.
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <img src={test2} alt="" />
-            <div>
-              <h4>Aron Fernandez</h4>
-              <p>Founder of WiTechs.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <img src={test3} alt="" />
-            <div>
-              <h4>Gimi Babez</h4>
-              <p>Founder of HelloTechs.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+        </div>
+        <div
+          className="flex flex-col items-center justify-center"
+          onClick={() => (toggleState === 3 ? toggleTab(2) : toggleTab(3))}
+        >
+          <img
+            src={toggleState === 3 ? test2 : test3}
+            alt=""
+            className="w-[112px] mt-9"
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- PixelPlo/softlight/src/data/homeData.js
+++ PixelPlo/softlight/src/data/homeData.js
@@ -5,7 +5,7 @@ import test3 from "../assets/test3.png";
 export const testimonialData = [
   {
     id: 1,
-    text: "High-end digital experiences.Created at the heart of Manhattan,we are a should human team.The driving force of all speeches, we believe that creation should be the point around which any strategy revolves.",
+    text: "Elevating digital innovation. Situated in the pulse of Silicon Valley, we are a dynamic collective of minds. Pioneering the forefront of technological advancement, we champion the ethos that innovation should be the cornerstone of every endeavor.",
     image: test1,
     name: "Charl Gems",
     designation: "Founder of HiTechs.",
@@ -19,7 +19,7 @@ export const testimonialData = [
   },
   {
     id: 3,
-    text: "High-end digital experiences.Created at the heart of Manhattan,we are a should human team.The driving force of all speeches, we believe that creation should be the point around which any strategy revolves.",
+    text: "Redefining luxury in the digital realm. Nestled amidst the cultural tapestry of Paris, we are a select cadre of visionaries. Leading the charge in crafting unparalleled digital opulence, we advocate that sophistication should be the nucleus of every digital narrative.",
     image: test3,
     name: "Gimi Babez",
     designation: "Founder of HelloTechs.",
