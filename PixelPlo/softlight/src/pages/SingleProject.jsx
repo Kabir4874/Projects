8c757dacc9e9694f5bad5e92e2d6c72a58ce59a1
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-07 22:01:01 +0600

    add

--- PixelPlo/softlight/src/pages/SingleProject.jsx
+++ PixelPlo/softlight/src/pages/SingleProject.jsx
@@ -1,10 +1,15 @@
-
+import AboutTheProject from "../components/singleProject/AboutTheProject";
 import HeroSection from "../components/singleProject/HeroSection";
+import MoreProject from "../components/singleProject/MoreProject";
+import ProjectGallery from "../components/singleProject/ProjectGallery";
 
 const SingleProject = () => {
   return (
     <div className="w-full overflow-x-hidden single-project-bg">
       <HeroSection />
+      <AboutTheProject />
+      <ProjectGallery />
+      <MoreProject />
     </div>
   );
 };
