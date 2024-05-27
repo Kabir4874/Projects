5017de84628c78381dfad1ee7b59b7bf25d5668c
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-27 22:14:41 +0600

    add

---
+++ multi-vendor-ecommerce/dashboard/src/views/auth/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="min-w-screen min-h-screen bg-mirage flex justify-center items-center">
+      <div className="w-[350px] text-iron p-2">
+        <div className=" bg-ebony_clay p-4 rounded-md">
+          <h2 className="text-xl mb-3">Welcome to E-commerce</h2>
+          <p className="text-sm mb-3">
+            Please Register and start your business
+          </p>
+          <form action="">
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Name</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-iron focus:border-indigo-500 overflow-hidden"
+                type="text"
+                name="name"
+                placeholder="name"
+                id="name"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-iron focus:border-indigo-500 overflow-hidden"
+                type="email"
+                name="email"
+                placeholder="email"
+                id="email"
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-700 bg-transparent rounded-md text-iron focus:border-indigo-500 overflow-hidden"
+                type="password"
+                name="password"
+                placeholder="password"
+                id="password"
+                required
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- multi-vendor-ecommerce/dashboard/tailwind.config.js
+++ multi-vendor-ecommerce/dashboard/tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mirage: "#161D31",
+        iron: "#D0D2D6",
+        ebony_clay: "#283046",
+      },
+    },
   },
   plugins: [],
-}
-
+};
