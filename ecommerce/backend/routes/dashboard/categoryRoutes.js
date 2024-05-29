2c962102a2805df0b16936b5bf2bc3bea79da49d
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-29 17:15:22 +0600

    add

--- ecommerce/backend/routes/dashboard/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { authMiddleware } = require("../../middlewares/authMiddleware");
-const categoryControllers = require("../../controllers/dashboard/categoryControllers");
-
-router.post("/category-add", authMiddleware, categoryControllers.add_category);
-router.get("/category-get", authMiddleware, categoryControllers.get_category);
-
-module.exports = router;
--- ecommerce/dashboard/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  // backend url
-  baseURL: "http://localhost:5000/api",
-});
-
-export default api;
